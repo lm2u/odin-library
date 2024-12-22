4524e345356c89ecf00a2ee7c31107f958cc3d9e
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-12-23 05:13:46 +0800

    Add delete button

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 //Empty array to store book objects
 const myLibrary = []
+let counter = 2;
 
 //Book constructor function
-function Book(title, author, year, pages, read){
+function Book(title, author, year, pages, read, counter){
   this.title = title;
   this.author = author;
   this.year = year;
   this.pages = pages;
   this.read = read;
+  this.counter = counter;
 }
 
 //Pass on parameters to the Book constructor
-function addBookToLibrary(title, author, year, pages, read){
-  const newBook = new Book(title, author, year, pages, read)
+function addBookToLibrary(title, author, year, pages, read, counter){
+  const newBook = new Book(title, author, year, pages, read, counter)
   myLibrary.push(newBook)
   return newBook
 }
@@ -23,6 +25,7 @@ const sectionWrapper = document.querySelector(".library-section")
 function displayBook(book){
   const cardWrapper = document.createElement("div")
   cardWrapper.classList.add("card-wrapper")
+  cardWrapper.dataset.id = counter
   sectionWrapper.appendChild(cardWrapper)
 
   const title = document.createElement("h3")
@@ -35,18 +38,24 @@ function displayBook(book){
   pages.classList.add("pages")
   const read = document.createElement("div")
   read.classList.add("read")
+  const delBtn = document.createElement("button")
+  delBtn.classList.add("delete")
+  delBtn.dataset.id = counter
+  delBtn.onclick = deleteBook(counter)
 
   title.textContent = `${book.title}`
   author.textContent = `${book.author}`
   year.textContent = `${book.year}`
   pages.textContent = `${book.pages}`
   read.textContent = `${book.read}`
+  delBtn.textContent = "Delete"
 
   cardWrapper.appendChild(title)
   cardWrapper.appendChild(author)
   cardWrapper.appendChild(year)
   cardWrapper.appendChild(pages)
   cardWrapper.appendChild(read)
+  cardWrapper.appendChild(delBtn)
 }
 
 //Event handler for the "submit" button
@@ -59,6 +68,18 @@ bookForm.addEventListener("submit",(event)=>{
   bookObj.year = bookForm.year.value
   bookObj.page = bookForm.page.value
   bookObj.read = bookForm.read.value
-  newBook = addBookToLibrary(bookObj.title, bookObj.author, bookObj.year, bookObj.page, bookObj.read)
+  newBook = addBookToLibrary(bookObj.title, bookObj.author, bookObj.year, bookObj.page, bookObj.read, counter)
   displayBook(newBook)
+  counter++
 })
+
+//Delete the card upon clicking.
+//Tracking based on the data-id value of card-wrapper
+function deleteBook(id) {
+  const btn = document.querySelector(`[data-id="${id}"]`)
+  btn.addEventListener("click",()=>{
+    removeId = btn.dataset.id
+    const card = document.querySelector(`[data-id="${removeId}"]`)
+    sectionWrapper.removeChild(card)
+  })
+}
