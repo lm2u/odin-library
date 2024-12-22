9195320d0e2870e7f9e6f044438f98ef9f2f4ff0
luke <142288087+lukemawuntu@users.noreply.github.com>
2024-12-22 21:27:33 +0800

    Add function to display book on page

--- script.js
+++ script.js
@@ -29,6 +29,35 @@ function addBookToLibrary(){
   myLibrary.push(newBook)
 }
 
+
+const sectionWrapper = document.querySelector(".library-section")
+
 myLibrary.forEach(book => {
-  console.log(book)
+  const cardWrapper = document.createElement("div")
+  cardWrapper.classList.add("card-wrapper")
+  sectionWrapper.appendChild(cardWrapper)
+
+  const title = document.createElement("h3")
+  title.classList.add("title")
+  const author = document.createElement("div")
+  author.classList.add("author")
+  const year = document.createElement("div")
+  year.classList.add("year")
+  const pages = document.createElement("div")
+  pages.classList.add("pages")
+  const read = document.createElement("div")
+  read.classList.add("read")
+
+  title.textContent = `${book.title}`
+  author.textContent = `${book.author}`
+  year.textContent = `${book.year}`
+  pages.textContent = `${book.pages}`
+  read.textContent = `${book.read}`
+
+  cardWrapper.appendChild(title)
+  cardWrapper.appendChild(author)
+  cardWrapper.appendChild(year)
+  cardWrapper.appendChild(pages)
+  cardWrapper.appendChild(read)
+
 });
